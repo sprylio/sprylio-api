ad172b9e50a89bc84527fc7cbb1670cd7df8c2f8
Richard Beauchamp <rbeauchamp@users.noreply.github.com>
2020-09-24 19:41:41 -0700

    #1

--- test/Sprylio.Api.Tests/GlobalSuppressions.cs
+++ test/Sprylio.Api.Tests/GlobalSuppressions.cs
@@ -4,3 +4,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "This is a unit test project.")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "Unit test method names are special.")]
--- test/Sprylio.Api.Tests/UnitTest1.cs
+++ test/Sprylio.Api.Tests/UnitTest1.cs
@@ -2,15 +2,50 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 
 namespace Sprylio.Api.Tests
 {
+    /// <summary>
+    /// See https://adamstorr.azurewebsites.net/blog/integration-testing-with-aspnetcore-3-1.
+    /// </summary>
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async Task get_weather_forecast_returns_set_of_forecasts()
         {
+            // Arrange
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    // Add TestServer
+                    webHost.UseTestServer();
+
+                    // Specify the environment
+                    webHost.UseEnvironment("Test");
+
+                    webHost.Configure(app => app.Run(async ctx => await ctx.Response.WriteAsync("Hello World!")));
+                });
+
+            // Create and start up the host
+            var host = await hostBuilder.StartAsync();
+
+            // Create an HttpClient which is setup for the test host
+            var client = host.GetTestClient();
+
+            // Act
+            var response = await client.GetAsync("/WeatherForecast");
+
+            // Assert
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.Should().Be("Hello World!");
         }
     }
 }
