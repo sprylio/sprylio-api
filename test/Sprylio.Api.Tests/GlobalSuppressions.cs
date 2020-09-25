07bad5bce228e707a43b822d9398c8e227531555
Richard Beauchamp <rbeauchamp@users.noreply.github.com>
2020-09-24 19:23:31 -0700

    #1

---
+++ test/Sprylio.Api.Tests/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Sprylio Inc. and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "This is a unit test project.")]
---
+++ test/Sprylio.Api.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Sprylio Inc. and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+
+namespace Sprylio.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
